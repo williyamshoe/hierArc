df80fb80d36abeb679033e3c23f30ee79d9daf2a
William Sheu <wiliam.sheu@berkeley.edu>
2024-05-12 14:31:45 -0700

    debugging

--- hierarc/LensPosterior/kin_constraints_composite_gradient.py
+++ hierarc/LensPosterior/kin_constraints_composite_gradient.py
@@ -32,6 +32,8 @@ class KinConstraintsCompositeGradient(KinConstraints):
         anisotropy_model,
         gamma_in_prior_mean=None,
         gamma_in_prior_std=None,
+        eta_prior_mean=None,
+        eta_prior_std=None,
         sigma_v_error_independent=None,
         sigma_v_error_covariant=None,
         sigma_v_error_cov_matrix=None,
@@ -168,6 +170,9 @@ class KinConstraintsCompositeGradient(KinConstraints):
 
         self._gamma_in_prior_mean = gamma_in_prior_mean
         self._gamma_in_prior_std = gamma_in_prior_std
+
+        self._eta_prior_mean = eta_prior_mean
+        self._eta_prior_std = eta_prior_std
 
         if not is_m2l_population_level and not self._check_arrays(
             self._kappa_s_array, log_m2l_array
@@ -421,6 +426,8 @@ class KinConstraintsCompositeGradient(KinConstraints):
             "param_scaling_grid_list": ani_scaling_grid_list,
             "gamma_in_prior_mean": self._gamma_in_prior_mean,
             "gamma_in_prior_std": self._gamma_in_prior_std,
+            "eta_prior_mean": self._eta_prior_mean,
+            "eta_prior_std": self._eta_prior_std,
         }
 
         if not self._is_m2l_population_level:
